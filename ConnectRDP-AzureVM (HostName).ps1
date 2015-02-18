c2029b91dc819f8be5237e08f1fffde764e9c7b1
Rakhesh Sasidharan <me@rakhesh.com>
2015-02-18 12:18:17 +0400

    Added my RDP scripts and also a VNet config XML file

---
+++ ConnectRDP-AzureVM (HostName).ps1
@@ -0,0 +1,13 @@
+﻿[CmdletBinding()]
+Param(
+[Parameter(Position=0, Mandatory=$true)]
+[string]$ServiceName
+)
+
+$VM = Get-AzureVM -ServiceName $ServiceName
+$VMPort =  (Get-AzureEndpoint -VM $VM -Name "Remote Desktop").Port
+if ($VMPort -eq $null) { $VMPort = (Get-AzureEndpoint -VM $VM -Name "RemoteDesktop").Port }
+
+$VMName = ([system.uri]$VM.DNSName).Host
+
+Start-Process "mstsc" -ArgumentList "/v ${VMName}:$VMPort /f"
