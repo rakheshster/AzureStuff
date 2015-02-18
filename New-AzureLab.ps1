4bfc09263629788136774b747e81cfc638652e62
Rakhesh Sasidharan <me@rakhesh.com>
2015-02-18 21:39:43 +0400

    Committing my Azure lab script.

---
+++ New-AzureLab.ps1
@@ -0,0 +1,15 @@
+﻿Import-Module Azure
+
+$AzureSubscription = "Visual Studio Ultimate with MSDN"
+$AzureLocation = "SouthEast Asia"
+$VNetConfigFile = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\AzureVNet.xml"
+$StorageAccount = "rakheshlocallyredundant"
+$StorageType = "Standard_LRS"
+
+# Affinity group name cannot have spaces
+$AzureAffinityGroup = $AzureLocation -replace "\s*",""
+
+New-AzureAffinityGroup -Name $AzureAffinityGroup -Description "$AzureLocation affinity group" -Location $AzureLocation
+Set-AzureVNetConfig -ConfigurationPath $VNetConfigFile
+New-AzureStorageAccount -StorageAccountName $StorageAccount -AffinityGroup $AzureAffinityGroup -Type $StorageType
+Set-AzureSubscription -CurrentStorageAccountName $StorageAccount -SubscriptionName $AzureSubscription
