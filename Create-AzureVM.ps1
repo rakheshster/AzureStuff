c9032c73f1c3d025fdfb3e1c5d695df077b30cb5
Rakhesh Sasidharan <me@rakhesh.com>
2015-02-20 15:04:41 +0400

    Added a .gitignore and a PowerShell script I had previous used to create VMs. Will update that later.

---
+++ Create-AzureVM.ps1
@@ -0,0 +1,22 @@
+﻿$VMName = ""
+$InstanceSize = "Basic_A1"
+$ImageName = "a699494373c04fc0bc8f2bb1389d6106__Win2K8R2SP1-Datacenter-201412.01-en.us-127GB.vhd"
+$Location = "Southeast Asia"
+
+$Domain = ""
+$Subnet= "RAXNET1-24"
+$IPAddress = "192.168.24.11"
+$VNet = "RAXNET1"
+$AdminPassword =""
+$DomainPassword = ""
+
+$VMConfig = New-AzureVMConfig -Name $VMName -InstanceSize $InstanceSize -ImageName $ImageName
+$VMConfig | Add-AzureProvisioningConfig -WindowsDomain -Domain $Domain -DomainUserName "rakhesh" -DomainPassword $DomainPassword -JoinDomain $Domain `
+            -DisableAutomaticUpdates -TimeZone "Arabian Standard Time" `
+            -AdminUsername "rakhesh" -Password $AdminPassword
+
+$VMConfig | Set-AzureSubnet -SubnetNames $Subnet
+$VMConfig | Set-AzureStaticVNetIP -IPAddress $IPAddress
+
+New-AzureService -ServiceName $VMName -Location $Location
+New-AzureVM -ServiceName $VMName -VNetName $VNet
