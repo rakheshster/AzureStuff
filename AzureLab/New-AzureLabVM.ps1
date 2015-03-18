80247e635fc191b6b6198bac46e370860215bb5d
Rakhesh Sasidharan <me@rakhesh.com>
2015-03-18 23:24:11 +0400

    New-AzureLabVM.ps1 changes complete. Needs testing.

--- AzureLab/New-AzureLabVM.ps1
+++ AzureLab/New-AzureLabVM.ps1
@@ -12,7 +12,7 @@ Param(
     [string]$VMName
 )
 
-# Yourun this script point it to the env file, a VM defns file, and specifying a VM you want to initialize. The script will then do the needful.
+# You run this script point it to the env file, a VM defns file, and specifying a VM you want to initialize. The script will then do the needful.
 
 # This script must be run with admin rights, if not do it for the user
 # Thanks to http://stackoverflow.com/a/11440595 for the snippet below
@@ -38,15 +38,10 @@ $AzureVMs = (Get-Content $AzureVMDefns) -join "`n" | ConvertFrom-Json
 # TODO: Put some error logic in here if the Azure module isn't available ...
 Import-Module Azure
 
-# Previously I had all these as variables; now that I have a JSON file I don't want to go around changing the references everywhere
-# So I'll just read these into the variables. 
-$AzureSubscription = $AzureEnv.Subscription
-$StorageAccount = $AzureEnv.StorageAccount
-$StorageType = $AzureEnv.StorageType
-$AzureLocation = $AzureEnv.Location
-
-# I am going to call the affinity group same as my location. It cannot have spaces, so remove these. 
-$AzureAffinityGroup = $AzureLocation -replace "\s*",""
+# Add your Azure account
+#TODO: What happens if this fails?
+Write-Host -ForegroundColor Green "Launching new window to get Azure credentials"
+Add-AzureAccount
 
 $VMAdminUser = Read-Host -Prompt ("VM Admin Username")
 $VMAdminPass = "Password"
@@ -59,48 +54,38 @@ while (($VMAdminPass.length -lt 8) -or ($VMAdminPass -notmatch "[~!#$%^&*()`]") 
 # I use this object later
 $VMCredObject = New-Object System.Management.Automation.PSCredential ($VMAdminUser, $(ConvertTo-SecureString $VMAdminPass -AsPlainText -Force))
 
-# change the block below ... instead of a loop now I must just find the VM from the hashtable, create it, get its cert .... no more looping
+# Create the VM config
+Write-Host -ForegroundColor Green -NoNewline "Creating VM: "
+$VM = $AzureVMs | ?{ $_.Name -eq $VMName }
+if ($VM -eq $null) { Write-Error "Unable to find VM $VMName!"; break } else { Write-Host -ForegroundColor Green "$($VM.Name)" }
 
-
-# Create the VMs
-Write-Host -ForegroundColor Green "Creating VMs"
-foreach ($VM in $AzureVMs) {
-    Write-Host -ForegroundColor Green "* $($VM.Name)"
-    $AzureVMConfig = New-AzureVMConfig -Name $VM.Name -InstanceSize $VMInstanceSize -ImageName $VMImageName |
-        Add-AzureProvisioningConfig -Windows -AdminUsername $VMAdminUser -Password $VMAdminPass -TimeZone $VMTimeZone -NoRDPEndpoint |
+$AzureVMConfig = New-AzureVMConfig -Name $VM.Name -InstanceSize $VM.InstanceSize -ImageName $VM.ImageName |
+        Add-AzureProvisioningConfig -Windows -AdminUsername $VMAdminUser -Password $VMAdminPass -TimeZone $VM.TimeZone -NoRDPEndpoint |
         Set-AzureSubnet -SubnetNames $VM.Subnet
     
-    if ($VM.IPAddr) {
-        if ($(Test-AzureStaticVNetIP -IPAddress $VM.IPAddr -VNetName $VM.AddrSpaceName).IsAvailable) {
-            $AzureVMConfig | Set-AzureStaticVNetIP -IPAddress $VM.IPAddr
-            Write-Host -ForegroundColor Green "`t Static IP $($VM.IPAddr) set"
-        } else {
-            Write-Error "`t You asked for a Static IP $($VM.IPAddr) to be set but it is not available."
-        }
+if ($VM.IPAddr) {
+    if ($(Test-AzureStaticVNetIP -IPAddress $VM.IPAddr -VNetName $VM.AddrSpaceName).IsAvailable) {
+        $AzureVMConfig | Set-AzureStaticVNetIP -IPAddress $VM.IPAddr
+        Write-Host -ForegroundColor Green "`t Static IP $($VM.IPAddr) set"
+    } else {
+        Write-Error "`t You asked for a Static IP $($VM.IPAddr) to be set but it is not available."
     }
-
-    Write-Host -ForegroundColor Green "`t Creating Service"
-    New-AzureService -ServiceName $VM.Name -AffinityGroup $AzureAffinityGroup -ErrorAction SilentlyContinue
-    Write-Host -ForegroundColor Green "`t Provisioning VM"
-    $AzureVMConfig | New-AzureVM -ServiceName $VM.Name -VNetName $VM.AddrSpaceName
 }
 
-# Loop again, this time to get the certificates
-Write-Host -ForegroundColor Green "Adding certificates to local store"
-foreach ($VM in $AzureVMs) {
-    Write-Host -ForegroundColor Green "* $($VM.Name)"
-    $AzureVMName = $VM.Name
-    (Get-AzureCertificate -ServiceName $AzureVMName).Data | Out-File "$env:TEMP\$AzureVMName.cer"
-    Import-Certificate -FilePath "$env:TEMP\$AzureVMName.cer" -CertStoreLocation Cert:\LocalMachine\root
-    Get-AzureWinRMUri -ServiceName $AzureVMName | ft Host,Port
-}
+# Create service & provision the VM
+Write-Host -ForegroundColor Green "Creating Service"
+New-AzureService -ServiceName $VM.Name -AffinityGroup $($AzureEnv.Location -replace "\s*","") -ErrorAction SilentlyContinue
+Write-Host -ForegroundColor Green "Provisioning VM"
+$AzureVMConfig | New-AzureVM -ServiceName $VM.Name -VNetName $VM.AddrSpaceName
+
+(Get-AzureCertificate -ServiceName $VM.Name).Data | Out-File "$env:TEMP\$($VM.Name).cer"
+Import-Certificate -FilePath "$env:TEMP\$($VM.Name).cer" -CertStoreLocation Cert:\LocalMachine\root
+Get-AzureWinRMUri -ServiceName $VM.Name | ft Host,Port
 
 # Adding roles
 Write-Host -ForegroundColor Green "Adding roles"
-foreach ($VM in $AzureVMs) {
-    Write-Host -ForegroundColor Green "* $($VM.Name)"
 
-    if ($VM.Role.Contains("Primary DC")) {
+if ($VM.Role -eq "Primary DC") {
     # Do first DC stuff here
     Write-Host -ForegroundColor Green "`t This is the first DC in the domain/ forest"
     $InstallAD = {
@@ -111,24 +96,23 @@ foreach ($VM in $AzureVMs) {
         
         Write-Host -ForegroundColor Green "`t Promoting to DC"
         Install-ADDSForest -DomainName AzureLab.local -DomainNetbiosName AzureLab -DomainMode Win2012R2 -ForestMode Win2012R2 -InstallDns -NoDnsOnNetwork -Force
-        }
+    }
     
     Invoke-Command -ConnectionUri $(Get-AzureWinRMUri -ServiceName $VM.Name -Name $VM.Name) -Credential $VMCredObject -ScriptBlock $InstallAD
     Write-Host -ForegroundColor Green "`t The machine may reboot and we will lose connection - don't panic!"
-    }
+}
 
-if ($VM.Role.Contains("DC")) {
+if ($VM.Role  -eq "DC") {
     # Do regular DC stuff here
     Write-Host -ForegroundColor Green "`t This is a regular DC+DNS"
+
     $InstallAD = {
         Write-Host -ForegroundColor Green "`t Installing role"
         Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -Restart
         
         Write-Host -ForegroundColor Green "`t Promoting to DC"
         Install-ADDSDomainController -InstallDns -DomainName AzureLab.local
-        }
+    }
     
     Invoke-Command -ConnectionUri $(Get-AzureWinRMUri -ServiceName $VM.Name -Name $VM.Name) -Credential $VMCredObject -ScriptBlock $InstallAD
-    }
-
 }
